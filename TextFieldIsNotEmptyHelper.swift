e30c710c13f99aa73b1cd624cb65eaa644332235
Gregg Jaskiewicz <gryzman@gmail.com>
2018-01-28 22:53:07 +0000

    add

---
+++ TextFieldIsNotEmptyHelper.swift
@@ -0,0 +1,33 @@
+//
+//  TextFieldIsNotEmptyHelper.swift
+//
+//  Created by Gregg Jaskiewicz on 26/01/2018.
+//  Copyright © 2018 Gregg Jaskiewicz. All rights reserved.
+//
+
+import Foundation
+
+final class TextFieldIsNotEmptyHelper: NSObject, UITextFieldDelegate {
+
+    private let minimalLength: Int
+    private let notificationCallback: (_: Bool)->()
+
+    init(minimalLength: Int = 1, notificationCallback: @escaping (_: Bool)->()) {
+        self.minimalLength = minimalLength
+        self.notificationCallback = notificationCallback
+        super.init()
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let userEnteredString = textField.text as NSString? else {
+            return true
+        }
+
+        let newString = userEnteredString.replacingCharacters(in: range, with: string) as NSString
+
+        let qualifies = newString.length > self.minimalLength
+        self.notificationCallback(qualifies)
+
+        return true
+    }
+}
---
+++ TextViewIsNotEmptyHelper.swift
@@ -0,0 +1,34 @@
+//
+//  TextViewIsNotEmptyHelper.swift
+//
+//  Created by Gregg Jaskiewicz on 26/01/2018.
+//  Copyright © 2018 Gregg Jaskiewicz. All rights reserved.
+//
+
+import Foundation
+
+final class TextViewIsNotEmptyHelper: NSObject, UITextViewDelegate {
+
+    private let minimalLength: Int
+    private let notificationCallback: (_: Bool)->()
+
+    init(minimalLength: Int = 1, notificationCallback: @escaping (_: Bool)->()) {
+        self.minimalLength = minimalLength
+        self.notificationCallback = notificationCallback
+        super.init()
+    }
+
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+
+        guard let userEnteredString = textView.text as NSString? else {
+            return true
+        }
+
+        let newString = userEnteredString.replacingCharacters(in: range, with: text) as NSString
+
+        let qualifies = newString.length > self.minimalLength
+        self.notificationCallback(qualifies)
+
+        return true
+    }
+}
